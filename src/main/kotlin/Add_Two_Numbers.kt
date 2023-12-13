2c910b6355255992d98f6bd35ad75d5f51eade12
Alper Sahin <alper_sahin20@erdogan.edu.tr>
2023-12-13 23:14:46 +0100

    Added Two Numbers

--- src/main/kotlin/Add_Two_Numbers.kt
+++ src/main/kotlin/Add_Two_Numbers.kt
@@ -1,28 +1,54 @@
 fun main(args: Array<String>) {
+    // Örnek kullanım
+    val l1 = ListNode(2)
+    l1.next = ListNode(4)
+    l1.next!!.next = ListNode(3)
 
-    val list1 = ListNode(1)
-    list1.next = ListNode(2)
-    list1.next?.next = ListNode(3)
+    val l2 = ListNode(5)
+    l2.next = ListNode(6)
+    l2.next!!.next = ListNode(4)
 
-    // İkinci bağlı liste örneği
-    val list2 = ListNode(4)
-    list2.next = ListNode(5)
-    list2.next?.next = ListNode(6)
+    val solution = Solution()
+    val result = solution.addTwoNumbers(l1, l2)
 
-    // İlk bağlı listeyi yazdır
-    print("List 1: ")
-    println(list1.toString())
-
-    // İkinci bağlı listeyi yazdır
-    print("List 2: ")
-    println(list2.)
-}
-
-
-fun addTwoNumbers(l1: ListNode?, l2: ListNode?): ListNode? {
-
+    // Sonucu yazdırma
+    var current = result
+    while (current != null) {
+        print("${current.`val`} ")
+        current = current.next
+    }
 }
 
 class ListNode(var `val`: Int) {
     var next: ListNode? = null
+}
+
+class Solution {
+    fun addTwoNumbers(l1: ListNode?, l2: ListNode?): ListNode? {//l1: val =2,  l2: val =5
+        var carry = 0
+        var dummy = ListNode(0) // dummy: val = 0 next = null
+        var current = dummy// current: val = 0 next = null
+        var p = l1 //l1: val =2 next = null
+        var q = l2 //l2: val =5 next = null
+
+        while (p != null || q != null) {
+            val x = p?.`val` ?: 0 // x = 2
+            val y = q?.`val` ?: 0 // y = 5
+
+            val sum = x + y + carry// sum = 7
+            carry = sum / 10 // carry = 7/10
+
+            current.next = ListNode(sum % 10) // current: val = 0 next = ListNode(sum % 10)
+            current = current.next!!// current: val = 0 next = null
+
+            if (p != null) p = p.next
+            if (q != null) q = q.next
+        }
+
+        if (carry > 0) {
+            current.next = ListNode(carry)
+        }
+
+        return dummy.next
+    }
 }
