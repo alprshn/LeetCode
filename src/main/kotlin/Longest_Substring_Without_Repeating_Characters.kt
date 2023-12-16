4d4b7f82d331f1941b8fad187d08e840062a38eb
Alper Sahin <alper_sahin20@erdogan.edu.tr>
2023-12-16 16:50:45 +0100

    Longest Substring Without Repeating Characters Sliding Window

--- src/main/kotlin/Longest_Substring_Without_Repeating_Characters.kt
+++ src/main/kotlin/Longest_Substring_Without_Repeating_Characters.kt
@@ -1,8 +1,8 @@
 import kotlin.math.max
 
 fun main(args: Array<String>) {
-    var s:String = "abcabcbb"
-    lengthOfLongestSubstring(s)
+    var s:String = "pwwkew"
+    lengthOfLongestSubstring2(s)
 
 }
 
@@ -15,13 +15,33 @@ fun lengthOfLongestSubstring(s: String): Int {
     for (windowEnd in s.indices) {
 
         val ch = s[windowEnd]
-
+        println(s[windowEnd])
         if (hashMap.contains(ch)) {
             windowStart = max(windowStart, hashMap[ch]?.plus(1) ?: 0)
         }
-        hashMap[ch] = windowEnd
+        hashMap[ch] = windowEnd //3
         longest = max(longest, windowEnd - windowStart + 1)
     }
-
     return longest
+}
+
+
+fun lengthOfLongestSubstring2(s: String): Int {
+    var i = 0
+    var j = 0
+    var max = 0
+    val  hashSet = mutableSetOf<Char>()
+    while (i < s.length){
+        if (!hashSet.contains(s[i])){
+            hashSet.add(s[i])
+            i++
+            max = max(max, hashSet.size)
+        }
+        else{
+            hashSet.remove(s[j])
+            j++
+        }
+    }
+    return max
+
 }
