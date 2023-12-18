6b4a2b3917cbf3b01f323d4a299500f531719fce
Alper Sahin <alper_sahin20@erdogan.edu.tr>
2023-12-18 23:20:48 +0100

    This class will edit because function is ready function

--- src/main/kotlin/Median_Of_Two_Sorted_Arrays.kt
+++ src/main/kotlin/Median_Of_Two_Sorted_Arrays.kt
@@ -3,14 +3,15 @@ import kotlin.math.roundToInt
 
 fun main(args: Array<String>) {
     var nums1 : IntArray = intArrayOf(1,2)
-    var nums2 : IntArray = intArrayOf(3)
+    var nums2 : IntArray = intArrayOf(3,4,7,6)
     findMedianSortedArrays(nums1,nums2)
 
 }
 
 fun findMedianSortedArrays(nums1: IntArray, nums2: IntArray): Double {
 
-    var a = nums1 + nums2
+    val a = nums1 + nums2
+    a.sort()
     if (a.size%2 == 0){
         var c:Double = ((a.size /2)-1).toDouble()
         var z: Double = (a[c.toInt()] + a[(c + 1.0).toInt()]) / 2.0
@@ -23,3 +24,5 @@ fun findMedianSortedArrays(nums1: IntArray, nums2: IntArray): Double {
     }
 
 }
+
+
