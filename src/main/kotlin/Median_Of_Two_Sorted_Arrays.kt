9ae6dd19c3799b33324f23b18d70777536d5a4a3
Alper Sahin <alper_sahin20@erdogan.edu.tr>
2023-12-18 22:52:56 +0100

    Median of Two Sorted Arrays Adding Function

--- src/main/kotlin/Median_Of_Two_Sorted_Arrays.kt
+++ src/main/kotlin/Median_Of_Two_Sorted_Arrays.kt
@@ -1,8 +1,9 @@
 import kotlin.math.max
+import kotlin.math.roundToInt
 
 fun main(args: Array<String>) {
     var nums1 : IntArray = intArrayOf(1,2)
-    var nums2 : IntArray = intArrayOf(3,4)
+    var nums2 : IntArray = intArrayOf(3)
     findMedianSortedArrays(nums1,nums2)
 
 }
@@ -10,12 +11,18 @@ fun main(args: Array<String>) {
 fun findMedianSortedArrays(nums1: IntArray, nums2: IntArray): Double {
 
     var a = nums1 + nums2
-    println(a)
-    for (x in 0 .. nums1.size-1){
-
+    println()
+    if (false){
+        var c:Double = ((a.size /2)-1).toDouble()
+        println(c)
+        var z: Double = (a[c.toInt()] + a[(c + 1.0).toInt()]) / 2.0
+        println(z)
+        return z
     }
-    for (j in 0 .. nums2.size-1){
-
+     else{
+        var f:Int = ((a.size /2))
+        var g: Double = a[f].toDouble()
+        println(g)
     }
     return -1.0
 }
