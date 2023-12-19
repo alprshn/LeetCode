46711aed73922ca613a7f9c07febd8ca5fa0a586
Alper Sahin <alper_sahin20@erdogan.edu.tr>
2023-12-19 19:11:53 +0100

    Longest Palindromic Substring

---
+++ src/main/kotlin/Longest_Palindromic_Substring.kt
@@ -0,0 +1,23 @@
+fun main(args: Array<String>) {
+
+    var numberList : IntArray = intArrayOf(2,7,11,15)
+
+    twoSum(numberList, 9)
+    println(twoSum(numberList, 9).joinToString())
+}
+
+fun twoSum(nums: IntArray, target: Int): IntArray {
+    val hashMap : HashMap<Int,Int> = hashMapOf()
+
+    for (i in 0.. nums.size){
+        val kalan : Int = target - nums[i]
+        if (hashMap.contains(kalan)){
+            return intArrayOf(hashMap[kalan]!!,i)
+        }
+        hashMap[nums[i]] = i
+    }
+    throw IllegalStateException("no pair found")
+}
+fun longestPalindrome(s: String): String {
+
+}
