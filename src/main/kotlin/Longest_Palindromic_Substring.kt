628c4820f93331c1068a737b47d6e61cf1dbc632
Alper Sahin <alper_sahin20@erdogan.edu.tr>
2023-12-19 21:38:23 +0100

    Longest Palindromic Substring

--- src/main/kotlin/Longest_Palindromic_Substring.kt
+++ src/main/kotlin/Longest_Palindromic_Substring.kt
@@ -8,42 +8,25 @@ fun main(args: Array<String>) {
 }
 
 fun longestPalindrome(s: String): String {
-    var fakeList: MutableList<String> = mutableListOf()
-    var resultList: MutableList<String> = mutableListOf()
     var a:String=""
     var maxi = 0
     var fakeMax=0
     var t = ""
-
     var f = 0
-   while (f < s.length){
+    while (f < s.length){
         for (z in f..s.length-1){
-            //fakeList.add(s[z].toString())
             t+= s[z]
             if (t == t.reversed()){
                 maxi = max(maxi, t.length)
                 if (maxi > fakeMax){
-                    //resultList.clear()
                     a=""
-
                     fakeMax = maxi
                     a = t
-
-
-                    //resultList = (resultList + fakeList).toMutableList()
                 }
             }
-
         }
-        fakeList.clear()
-       t = ""
+        t = ""
         f++
     }
-
-    println(a)
-    //for (v in resultList){
-        //a += v
-    //}
-    //println(a)
     return a
 }
