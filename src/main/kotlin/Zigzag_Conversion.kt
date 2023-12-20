ce59fa58922422c822e8f0ff766e95b554a1fd99
Alper Sahin <alper_sahin20@erdogan.edu.tr>
2023-12-20 21:53:40 +0100

    Zigzag Conversion

--- src/main/kotlin/Zigzag_Conversion.kt
+++ src/main/kotlin/Zigzag_Conversion.kt
@@ -5,9 +5,28 @@ fun main(args: Array<String>) {
     var string : String = "PAHNAPLSIIGYIR"
     var numRows: Int = 3
     convert(string,numRows)
-    println( longestPalindromeSlidingWindow(string))
 }
 
 fun convert(s: String, numRows: Int): String {
+
+    var j =""
+
+    for (x in 1 until s.length ){
+        //println(x)
+        var f:Int = ((numRows * 2)-1) + x
+        j += s[x] + s[f].toString()
+
+        if (x == 1 || x == 6){
+            println(j)
+        }
+        if (x != 1 ||  x != 6){
+            println("_")
+        }
+    }
+
+    var a =0
+    while (a < s.length){
+
+    }
     return ""
 }
