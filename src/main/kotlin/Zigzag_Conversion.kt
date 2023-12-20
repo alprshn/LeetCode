01563f7942363f4efd5d1b25c4d2919c5e051c83
Alper Sahin <alper_sahin20@erdogan.edu.tr>
2023-12-20 21:17:54 +0100

    Zigzag Conversion

---
+++ src/main/kotlin/Zigzag_Conversion.kt
@@ -0,0 +1,13 @@
+import kotlin.math.max
+
+fun main(args: Array<String>) {
+
+    var string : String = "PAHNAPLSIIGYIR"
+    var numRows: Int = 3
+    convert(string,numRows)
+    println( longestPalindromeSlidingWindow(string))
+}
+
+fun convert(s: String, numRows: Int): String {
+    return ""
+}
