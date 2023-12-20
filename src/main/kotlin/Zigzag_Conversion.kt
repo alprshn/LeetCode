fd9f5f9f05252f72a46020aba18fdc425590d08d
Alper Sahin <alper_sahin20@erdogan.edu.tr>
2023-12-20 23:06:59 +0100

    Zigzag Conversion

--- src/main/kotlin/Zigzag_Conversion.kt
+++ src/main/kotlin/Zigzag_Conversion.kt
@@ -2,8 +2,8 @@ import kotlin.math.max
 
 fun main(args: Array<String>) {
 
-    var string : String = "PAHNAPLSIIGYIR"
-    var numRows: Int = 3
+    var string : String = "PAYPALISHIRING"
+    var numRows: Int = 4
     convert(string,numRows)
 }
 
@@ -11,17 +11,22 @@ fun convert(s: String, numRows: Int): String {
 
     var j =""
 
-    for (x in 1 until s.length ){
+    for (x in 0 until numRows){
         //println(x)
         //var f:Int = ((numRows * 2)-1) + x
         //j += s[x] + s[f].toString()
+        var e = (numRows-2)*2
+        var z = 0
         var r = x
-        while (r < s.length-1){
-            j += s[r]
-            r += ((numRows * 2) - 1)
-
+        while (r <= s.length-1 && r >= 0){
+                j += s[r]
+                r += ((numRows * 2) - 2)
         }
+        e -=2
+        z +=2
+
         println(j)
+
         j = ""
 
     }
