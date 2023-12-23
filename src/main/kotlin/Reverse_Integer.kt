bdd089c2fd75f8e6168cc7197d8cf77ac1f71de7
Alper Sahin <alper_sahin20@erdogan.edu.tr>
2023-12-23 23:53:45 +0100

    Reverse Integer

--- src/main/kotlin/Reverse_Integer.kt
+++ src/main/kotlin/Reverse_Integer.kt
@@ -8,22 +8,10 @@ fun main(args: Array<String>) {
 }
 
 fun reverse(x: Int): Int {
-    if (numRows == 1) {
-        return s
-    }
+    var stringNumber: String = x.toString()
 
-    val result = StringBuilder()
-    val n = s.length
-    val interval = 2 * numRows - 2
 
-    for (i in 0 until numRows) {
-        for (j in i until n step interval) {
-            result.append(s[j])
-            if (i != 0 && i != numRows - 1 && j + interval - 2 * i < n) {
-                result.append(s[j + interval - 2 * i])
-            }
-        }
-    }
-    println(result.toString())
-    return result.toString()
+
+
+    return 0
 }
