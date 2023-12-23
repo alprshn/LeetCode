dddfea3d7f856b730aede2dce945ea49056a2c7a
Alper Sahin <alper_sahin20@erdogan.edu.tr>
2023-12-23 23:59:00 +0100

    Reverse Integer

--- src/main/kotlin/Reverse_Integer.kt
+++ src/main/kotlin/Reverse_Integer.kt
@@ -4,13 +4,20 @@ fun main(args: Array<String>) {
 
     var string : String = "PAYPALISHIRING"
     var numRows: Int = 4
-    convert(string,numRows)
+    reverse(-100)
 }
 
 fun reverse(x: Int): Int {
     var stringNumber: String = x.toString()
-
-
+    var c = stringNumber.length-1
+    var result:String= ""
+    while (0 < stringNumber.length-1){
+        if (stringNumber[c] != '-'){
+            result += stringNumber[c]
+        }
+        c--
+    }
+    println(result)
 
 
     return 0
