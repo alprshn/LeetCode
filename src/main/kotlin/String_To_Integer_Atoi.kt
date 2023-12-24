f03d7a469fff112349d105c2bf1d18a19a59f5d5
Alper Sahin <alper_sahin20@erdogan.edu.tr>
2023-12-24 20:31:56 +0100

    String To Integer (atoi)

--- src/main/kotlin/String_To_Integer_Atoi.kt
+++ src/main/kotlin/String_To_Integer_Atoi.kt
@@ -1,15 +1,21 @@
 fun main(args: Array<String>) {
 
-    var a :String = "Alper100"
+    var a :String = "Alper100asda1212"
     myAtoi(a)
 }
 
 fun myAtoi(s: String): Int {
     var result = ""
-    for (i in s){
-        if (i.isDigit()){
-            result += i
+    for (i in 0 until s.length){
+
+        if (s[i].isDigit()){
+            result += s[i]
+            //bir i döngüsü oluştur  buraya girince artsın
+            if (!s[i+1].isDigit()){
+                break
+            }
         }
+        //bir i döngüsü oluştur  buraya gelince azalsın
     }
     println(result)
     return 0
