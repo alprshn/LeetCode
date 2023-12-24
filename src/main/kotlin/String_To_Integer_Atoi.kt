70a268c2cf41aec5e34aab8d7f55f357e5be12f6
Alper Sahin <alper_sahin20@erdogan.edu.tr>
2023-12-24 21:11:09 +0100

    String To Integer (atoi)

--- src/main/kotlin/String_To_Integer_Atoi.kt
+++ src/main/kotlin/String_To_Integer_Atoi.kt
@@ -1,14 +1,19 @@
 fun main(args: Array<String>) {
 
-    var a :String = "Alper+0100asda1212"
+    var a :String = "+-12"
     println(myAtoi(a))
 }
 
 fun myAtoi(s: String): Int {
     var result = ""
+    if (s.isEmpty() || s.length == 1 && !s[0].isDigit()) return 0
+
     if (s[0].isDigit() || (s[0] == ' '|| s[0] == '+'|| s[0] == '-')){
         for (i in 0 until s.length){
-
+            if (s[i] == '+' && s[i+1] == '-' ||s[i] == '-' && s[i+1] == '+'||s[i] == '-' && s[i+1] == '-'||s[i] == '+' && s[i+1] == '+'||s[i] == ' ' && s[i+1] == ' '){
+                return 0
+                break
+            }
             if (s[i].isDigit() || s[i] == '+'|| s[i] == '-' ){
                 result += s[i]
                 //bir i döngüsü oluştur  buraya girince artsın
@@ -22,5 +27,11 @@ fun myAtoi(s: String): Int {
     else{
         return 0
     }
-    return result.toInt()
+    try {
+        // Check if the result is within the valid range for integers
+        return result.toInt()
+    } catch (e: NumberFormatException) {
+        // Handle the case where the conversion exceeds the integer range
+        return if (result[0] == '-') Int.MIN_VALUE else Int.MAX_VALUE
+    }
 }
