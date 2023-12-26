067586e7e94c6bc571c1005984a15d34bf5de645
Alper Sahin <alper_sahin20@erdogan.edu.tr>
2023-12-26 11:48:01 +0100

    Palindrome Number

--- src/main/kotlin/Palindrome_Number.kt
+++ src/main/kotlin/Palindrome_Number.kt
@@ -1,8 +1,8 @@
 fun main(args: Array<String>) {
 
-    var a:Boolean = isPalindromeBruteForce(122)
+    var a: Boolean = isPalindromeBruteForce(122)
     println(a)
-   // var b:Boolean = isPalindromeSlidingWindow(121)
+    // var b:Boolean = isPalindromeSlidingWindow(121)
 
     //println(a.toString())
 }
@@ -10,20 +10,23 @@ fun main(args: Array<String>) {
 fun isPalindromeBruteForce(x: Int): Boolean {
     var x = x.toString()
     var resultLeft = ""
-    var resultRight =""
-    var j: Int = x.length-1
-    for (i in 0 until  x.length ){
+    var resultRight = ""
+    var j: Int = x.length - 1
+    for (i in 0 until x.length) {
         resultLeft += x[i]
     }
 
-    while (j >= 0){
+    while (j >= 0) {
         resultRight += x[j]
         j--
     }
     println(resultLeft)
 
     println(resultRight)
-    return if (resultRight == resultLeft) true else true
+
+    return if (resultRight == resultLeft) return true else false
+
+
 }
 
 fun isPalindromeSlidingWindow(x: Int): Boolean {
