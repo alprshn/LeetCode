38fae6533ebe97f317d50014622c5cf406d34c1b
Alper Sahin <alper_sahin20@erdogan.edu.tr>
2023-12-26 11:33:55 +0100

    Palindrome Number

--- src/main/kotlin/Palindrome_Number.kt
+++ src/main/kotlin/Palindrome_Number.kt
@@ -1,10 +1,17 @@
 fun main(args: Array<String>) {
 
-    var a:Boolean = isPalindrome(121)
+    var a:Boolean = isPalindromeBruteForce(121)
+    var b:Boolean = isPalindromeSlidingWindow(121)
+
     println(a.toString())
 }
 
-fun isPalindrome(x: Int): Boolean {
+fun isPalindromeBruteForce(x: Int): Boolean {
 
     return false
+}
+
+fun isPalindromeSlidingWindow(x: Int): Boolean {
+
+    return true
 }
