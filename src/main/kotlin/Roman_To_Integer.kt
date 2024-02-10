25ba36aa4ac6fb98dda2176e328314cda585d593
Alper Sahin <alper_sahin20@erdogan.edu.tr>
2024-02-11 00:24:08 +0100

    Roman_To_Integer

--- src/main/kotlin/Roman_To_Integer.kt
+++ src/main/kotlin/Roman_To_Integer.kt
@@ -1,48 +1,40 @@
 fun main(args: Array<String>) {
 
-    var a = romanToInt("LVIII")
+    var a = romanToInt("III")
     println(a.toString())
 }
 
 fun romanToInt(s: String): Int {
     val hashMap = HashMap<Char, Int>()
-    hashMap['I']= 1
-    hashMap['V']= 5
-    hashMap['X']= 10
-    hashMap['L']= 50
-    hashMap['C']= 100
-    hashMap['D']= 500
-    hashMap['M']= 1000
+    hashMap['I'] = 1
+    hashMap['V'] = 5
+    hashMap['X'] = 10
+    hashMap['L'] = 50
+    hashMap['C'] = 100
+    hashMap['D'] = 500
+    hashMap['M'] = 1000
     var result: Int = 0
-
-
-
     var i = 0
     while (i < s.length) { // Changed to while loop
         val romanNumber = s[i]
-        if (hashMap.contains(romanNumber)) {
-            var number = 0
-            if (s[i] == 'I' && (s[i+1] == 'V'|| s[i+1] == 'X')){
-                number = hashMap[s[i+1]]?.minus(hashMap[romanNumber]!!)!!
-                result += number
-                i += 1
+        if (i < s.length - 1) {
+            if (hashMap.containsKey(romanNumber)) {
+                val nextRomanNumber = s[i + 1]
+                if ((romanNumber == 'I' && (nextRomanNumber == 'V' || nextRomanNumber == 'X'))
+                    || (romanNumber == 'X' && (nextRomanNumber == 'L' || nextRomanNumber == 'C'))
+                    || (romanNumber == 'C' && (nextRomanNumber == 'D' || nextRomanNumber == 'M'))
+                ) {
+                    result += hashMap[nextRomanNumber]!! - hashMap[romanNumber]!!
+                    i++
+                } else {
+                    result += hashMap[romanNumber]!!
+                }
             }
-            if (s[i] == 'X' && (s[i+1] == 'L'|| s[i+1] == 'C')){
-                number = hashMap[s[i+1]]?.minus(hashMap[romanNumber]!!)!!
-                result += number
-                i += 1
-            }
-            if (s[i] == 'C' && (s[i+1] == 'D'|| s[i+1] == 'M')){
-                number = hashMap[s[i+1]]?.minus(hashMap[romanNumber]!!)!!
-                result += number
-                i += 1
-            }
-            else{
-                result += hashMap[romanNumber]!!
-            }
+        } else {
+            result += hashMap[romanNumber]!!
         }
-
         i++
+
     }
 
 
