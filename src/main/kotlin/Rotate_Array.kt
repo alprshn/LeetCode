6e21bd0970488006474cd38c7270026f852dd6cd
Alper Sahin <alper_sahin20@erdogan.edu.tr>
2024-08-13 21:27:49 +0300

    Rotate Array

---
+++ src/main/kotlin/Rotate_Array.kt
@@ -0,0 +1,12 @@
+fun main(args: Array<String>) {
+
+    var a: IntArray = intArrayOf(1, 1, 2, 2, 3)
+    var b: Int = 0
+    rotate(a, b)
+    println(rotate(a,b).toString())
+}
+
+fun rotate(nums: IntArray, k: Int): Unit {
+
+}
+
